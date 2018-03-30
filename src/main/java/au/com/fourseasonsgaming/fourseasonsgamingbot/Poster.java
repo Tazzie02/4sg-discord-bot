10b33e1571a781c46635952935de4893ade4bb62
Tazzie02 <Tazzie02@users.noreply.github.com>
2018-03-30 16:25:22 +1030

    Added sl4j logging and changed Poster to read new format

--- src/main/java/au/com/fourseasonsgaming/fourseasonsgamingbot/Poster.java
+++ src/main/java/au/com/fourseasonsgaming/fourseasonsgamingbot/Poster.java
@@ -5,6 +5,8 @@ import java.net.URL;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.tazzie02.tazbotdiscordlib.SendMessage;
 
@@ -14,8 +16,15 @@ import net.dv8tion.jda.core.entities.TextChannel;
 
 public class Poster {
 	
+	private final Logger logger = LoggerFactory.getLogger(Poster.class);
+	
 	private final JDA jda;
 	private final URL url;
+	private final String noUpdates;
+	
+	{
+		noUpdates = "NO_UPDATES";
+	}
 
 	public Poster(JDA jda, URL url) {
 		this.jda = jda;
@@ -25,13 +34,18 @@ public class Poster {
 	public void post() {
 		try {
 			String content = WebPage.download(url);
+			if (content.equals(noUpdates)) {
+				System.out.println("[POSTER]: No updates.");
+				return;
+			}
+			
 			JSONObject json = new JSONObject(content);
-//			System.out.println(json);
+			logger.debug("{}", json);
 			
 			String output = "";
 			for (String key : json.keySet()) {
-				String value = json.getString(key);
-				output += value + '\n';
+				JSONObject value = json.getJSONObject(key);
+				output += formatArticle(value) + '\n';
 			}
 			
 			SendMessage.sendMessage(getPostChannel(), output);
@@ -39,8 +53,19 @@ public class Poster {
 			System.out.println("Could not read webpage.");
 //			e.printStackTrace();
 		} catch (JSONException e) {
-			System.out.println("Could not parse JSON.");
+			System.out.println("Could not parse response as JSON.");
 		}
+	}
+	
+	private String formatArticle(JSONObject json) {
+		String articleType = json.getString("Article_Type");
+		String articleGame = json.getString("Article_Game");
+		String postedBy = json.getString("Posted_By");
+		String articleTitle = json.getString("Article_Title");
+		String articleUrl = json.getString("Article_URL");
+		
+		String formatted = String.format("%s (%s) posted by %s for %s (<%s>)", articleTitle, articleType, postedBy, articleGame, articleUrl); 
+		return formatted;
 	}
 	
 	private TextChannel getPostChannel() {
