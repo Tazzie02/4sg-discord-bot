3a5babe1ebbe8163d264ac8c5458702f6cdc40ea
Justin Tazzie <Tazzie02@users.noreply.github.com>
2019-01-20 14:40:23 +1030

    Added functionality to run roles command on a specified server ID

--- src/main/java/au/com/fourseasonsgaming/fourseasonsgamingbot/commands/RolesCommand.java
+++ src/main/java/au/com/fourseasonsgaming/fourseasonsgamingbot/commands/RolesCommand.java
@@ -2,6 +2,7 @@ package au.com.fourseasonsgaming.fourseasonsgamingbot.commands;
 
 import com.tazzie02.tazbotdiscordlib.Command;
 import com.tazzie02.tazbotdiscordlib.SendMessage;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -12,8 +13,29 @@ public class RolesCommand implements Command {
 
     @Override
     public void onCommand(MessageReceivedEvent e, String[] args) {
-        String output = String.format("List of roles and IDs in %s:\n", e.getGuild().getName());
-        for (Role role : e.getGuild().getRoles()) {
+        Guild guild;
+        if (args.length == 0) {
+            if (e.getGuild() == null) {
+                SendMessage.sendMessage(e, "Error: This command cannot be run in a private channel without specifying a server ID.");
+                return;
+            }
+            guild = e.getGuild();
+        }
+        else if (args.length == 1) {
+            guild = e.getJDA().getGuildById(args[0]);
+        }
+        else {
+            SendMessage.sendMessage(e, "Error: Incorrect number of arguments. Expected none or <serverId>.");
+            return;
+        }
+
+        if (guild == null) {
+            SendMessage.sendMessage(e, "Error: Could not find server with the specified ID.");
+            return;
+        }
+
+        String output = String.format("List of roles and IDs in %s:\n", guild.getName());
+        for (Role role : guild.getRoles()) {
             output += String.format(" - %s (%s)\n", role.getName(), role.getId());
         }
 
@@ -42,7 +64,8 @@ public class RolesCommand implements Command {
 
     @Override
     public String getDetails() {
-        return "roles - output list of roles.";
+        return "roles - output list of roles.\n" +
+                "roles [id] - output list of roles in guild with [id].";
     }
 
     @Override
